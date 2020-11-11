ec734e1c958f57d5330d6b8ac92de78c3f9bda71
pavelklos <pavelkloscz@gmail.com>
2020-11-11 17:30:48 +0100

    Updated 404.js

---
+++ src/pages/404.js
@@ -0,0 +1,11 @@
+import React from "react"
+import PrimaryLayout from "../layouts/PrimaryLayout"
+
+export default function NotFound() {
+  return (
+    <PrimaryLayout>
+      <h1>Page Not Found</h1>
+      <p>Oops, we couldn't find this page!</p>
+    </PrimaryLayout>
+  )
+}
